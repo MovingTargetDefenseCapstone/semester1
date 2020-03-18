69ea7ba4dbe79620f6ea2df5864b055843c3cb8b
vagrant <vagrant@metasploitable3-ub1404>
2020-03-18 20:54:28 +0000

    modified database migration script

--- database_migration.sh
+++ database_migration.sh
@@ -1,4 +1,7 @@
 mysqldump --compatible=postgresql --default-character-set=utf8 -r payroll.mysql -u root payroll -p sploitme
+wget https://raw.github.com/lanyrd/mysql-postgresql-converter/master/db_converter.py
+chmod +rx db_converter.py
+sudo apt-get install postgresql-9.3
 python db_converter.py payroll.mysql payroll.psql
 sudo -u postgres createdb payroll
 
