656b0dd32b54dbcadb0715034fa49561d9a796f5
Harrison Pratt <hpratt99@gmail.com>
2020-03-24 18:00:03 -0500

    Update database_migration.sh

--- database_migration.sh
+++ database_migration.sh
@@ -20,5 +20,8 @@ mysql -uroot -psploitme -e "CREATE DATABASE payroll;"
 mysql -uroot -psploitme payroll < payroll.mysql
 sudo cp ~/semester1/mysql_payroll_app.php /var/www/html/payroll_app.php
 
+# drop postgres database
+sudo -u postgres dropdb payroll
+
 # code to change ip address
 # sudo ifconfig eth0 xxx.xxx.xx.xx
