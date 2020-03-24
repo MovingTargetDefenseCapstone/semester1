3d851b91e24e6a46bf657a234155f4c1d7ff1add
Harrison Pratt <hpratt99@gmail.com>
2020-03-23 21:46:46 -0500

    Update database_migration.sh

--- database_migration.sh
+++ database_migration.sh
@@ -9,7 +9,13 @@ sudo -u postgres createdb payroll
 sudo nano /etc/postgresql/9.3/main/pg_hba.conf  # everywhere it says peer change to trust
 sudo service postgresql restart # try doing this before the previous step to see if it works
 psql payroll -U postgres -f payroll.psql
+# drop mysql payroll database
+# edit /var/www/html/payroll_app.php
 
+# rebuild mysql database
+# create mysql database payroll
+mysql -u root -p payroll < payroll.mysql
+# enter sploitme for password
 
 # code to change ip address
 sudo ifconfig eth0 xxx.xxx.xx.xx
