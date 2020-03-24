4ce7216dde6fd1aadd535f32d3e08672b3a1f453
vagrant <vagrant@metasploitable3-ub1404>
2020-03-24 00:00:55 +0000

    added command

--- database_migration.sh
+++ database_migration.sh
@@ -7,6 +7,10 @@ sudo apt-get install postgresql-9.3
 python db_converter.py payroll.mysql payroll.psql
 sudo -u postgres createdb payroll
 # one more command needed
+sudo nano /etc/postgresql/9.3/main/pg_hba.conf  # everywhere it says peer change to trust
+sudo service postgresql restart # try doing this before the previous step to see if it works
+psql payroll -U postgres 
+
 
 # code to chane ip address
 sudo ifconfig eth0 xxx.xxx.xx.xx
