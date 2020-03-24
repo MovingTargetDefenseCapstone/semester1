795049f01236f81111b382fb0429e7d5e211ea54
Harrison Pratt <hpratt99@gmail.com>
2020-03-23 20:59:55 -0500

    include payroll.psql file

--- database_migration.sh
+++ database_migration.sh
@@ -8,7 +8,7 @@ python db_converter.py payroll.mysql payroll.psql
 sudo -u postgres createdb payroll
 sudo nano /etc/postgresql/9.3/main/pg_hba.conf  # everywhere it says peer change to trust
 sudo service postgresql restart # try doing this before the previous step to see if it works
-psql payroll -U postgres 
+psql payroll -U postgres -f payroll.psql
 
 
 # code to change ip address
