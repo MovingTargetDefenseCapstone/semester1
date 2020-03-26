d13619008aa9e01a721ae2fbeefc2d26d2a74606
vagrant <vagrant@metasploitable3-ub1404>
2020-03-26 03:11:31 +0000

    update postgres php

--- postgres_payroll_app.php
+++ postgres_payroll_app.php
@@ -1,9 +1,7 @@
 <?php
 
-$conn = pg_connect("dbname=payroll user=postgres");
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+$conn = pg_connect("dbname=payroll user=postgres") or die("Could not connect");
+
 ?>
 
 <?php
@@ -35,30 +33,27 @@ if (!isset($_POST['s'])) {
 if($_POST['s']){
     $user = $_POST['user'];
     $pass = $_POST['password'];
-    $sql = "select username, first_name, last_name, salary from users where username = '$user' and password = '$pass'";
+    $sql = "select username, first_name, last_name, salary from users where username = '$user' and password = '$pass';";
+    $response = pg_query($conn, $sql);
+    $result = pg_fetch_all($response);
 
-    if ($conn->multi_query($sql)) {
-        do {
-            /* store first result set */
-            echo "<center>";
-            echo "<h2>Welcome, " . $user . "</h2><br>";
-            echo "<table style='border-radius: 25px; border: 2px solid black;' cellspacing=30>";
-            echo "<tr><th>Username</th><th>First Name</th><th>Last Name</th><th>Salary</th></tr>";
-            if ($result = $conn->store_result()) {
-                while ($row = $result->fetch_assoc()) {
-                    $keys = array_keys($row);
-                    echo "<tr>";
-                    foreach ($keys as $key) {
-                        echo "<td>" . $row[$key] . "</td>";
-                    }
-                    echo "</tr>\n";
-                }
-                $result->free();
-            }
-            if (!$conn->more_results()) {
-                echo "</table></center>";
-            }
-        } while ($conn->next_result());
+    echo "<center>";
+    echo "<h2>Welcome, " . $user . "</h2><br>";
+    echo "<table style='border-radius: 25px; border: 2px solid black;' cellspacing=30>";
+    echo "<tr><th>Username</th><th>First Name</th><th>Last Name</th><th>Salary</th></tr>";
+    if ($result){
+	for ($i = 0; $i < count($result); $i++){
+	     echo "<tr>";
+	     echo "<td>" . $result[$i][username] . "</td>";
+	     echo "<td>" . $result[$i][first_name] . "</td>";
+	     echo "<td>" . $result[$i][last_name] . "</td>";
+	     echo "<td>" . $result[$i][salary] . "</td>";
+	     echo "</tr>";
+	}
     }
+    echo "</table></center>";
+    pg_close($conn);
 }
 ?>
+
+
