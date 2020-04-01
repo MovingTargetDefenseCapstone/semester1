00202dc749cd8ff4061771b9a911df9911e14e71
vagrant <vagrant@metasploitable3-ub1404>
2020-04-01 23:37:44 +0000

    remover set up script

--- set_up_server_migration.sh
+++
@@ -1,4 +0,0 @@
-sudo cp ~/semester1/my-serve-cgi-bin.conf /etc/apache2/conf-available/serve-cgi-bin.conf
-sudo cp ~/semester1/my-apache2.conf /etc/apache2/apache2.conf
-sudo service apache2 restart
-
