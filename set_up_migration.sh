6b06b3e3113bf9469acfb82476b6b34867ec0b4b
vagrant <vagrant@metasploitable3-ub1404>
2020-04-01 21:35:04 +0000

    consolidated set up

--- set_up_migration.sh
+++ set_up_migration.sh
@@ -6,4 +6,6 @@ sudo chmod +rw ~/semester1/my_pg_hba.conf
 sudo service postgresql restart
 sudo apt-get -y install phpmyadmin
 sudo apt-get install php5-pgsql
+sudo cp ~/semester1/my-serve-cgi-bin.conf /etc/apache2/conf-available/serve-cgi-bin.conf
+sudo cp ~/semester1/my-apache2.conf /etc/apache2/apache2.conf
 sudo service apache2 restart
