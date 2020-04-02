421ea067ccb120c87a1918317a23d974b5e27f44
vagrant <vagrant@metasploitable3-ub1404>
2020-04-02 19:32:36 +0000

    update setup

--- set_up_migration.sh
+++ set_up_migration.sh
@@ -11,5 +11,5 @@ sudo pip install mysql-connector-python
 sudo cp ~/semester1/my-serve-cgi-bin.conf /etc/apache2/conf-available/serve-cgi-bin.conf
 sudo cp ~/semester1/my-apache2.conf /etc/apache2/apache2.conf
 sudo service apache2 restart
-sudo apt-get install python2.7-dev libmysqlclient-dev
+sudo apt-get -y install python2.7-dev libmysqlclient-dev
 sudo pip install psycopg2
