9ea6da8a922bc9c712aa343320bf731fc49b41b3
Harrison Pratt <hpratt99@gmail.com>
2020-04-02 15:29:53 -0500

    Delete postgres_to_mysql.sh

--- postgres_to_mysql.sh
+++
@@ -1,7 +0,0 @@
-# create mysql database
-mysql -uroot -psploitme -e "CREATE DATABASE payroll;" 
-mysql -uroot -psploitme payroll < payroll.mysql
-sudo cp ~/semester1/mysql_payroll_app.php /var/www/html/payroll_app.php
-
-# drop postgres database
-sudo -u postgres dropdb payroll
