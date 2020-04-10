8e6afa8a626b9ccece23e14be46113a0b461d7d1
vagrant <vagrant@metasploitable3-ub1404>
2020-04-10 19:47:00 +0000

    removed file

--- mysql_py_to_mysql_php.sh
+++
@@ -1,5 +0,0 @@
-sudo cp ~/semester1/mysql_payroll_app.php /var/www/html/payroll_app.php
-sudo rm /var/www/cgi-bin/payroll_app.py
-
-echo "$(date)   -->    MySQL, PHP"
-echo "$(date)   -->    MySQL, PHP" >> ~/configuration_log.txt
