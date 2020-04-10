eedf6faf265a233b860c48850bef3ac9e005261f
vagrant <vagrant@metasploitable3-ub1404>
2020-04-10 19:48:58 +0000

    removed migration files

--- mysql_php_to_mysql_py.sh
+++
@@ -1,5 +0,0 @@
-sudo cp ~/semester1/mysql_payroll_app.py /var/www/cgi-bin/payroll_app.py
-sudo rm /var/www/html/payroll_app.php
-
-echo "$(date)   -->    MySQL, Python"
-echo "$(date)   -->    MySQL, Python" >> ~/configuration_log.txt
