f030b46b98313ba9dbf93301e811c90ebd005f33
vagrant <vagrant@metasploitable3-ub1404>
2020-04-26 05:25:07 +0000

    weighted choices

--- migration_scripts/defender_loop.py
+++ migration_scripts/defender_loop.py
@@ -12,13 +12,25 @@ T = [3, 4, 3, 5]
 
 names = {0: 'mysql_php', 1: 'mysql_py', 2:'pg_php', 3:'pg_py'}
 
+def make_choice(choices):
+   total_weight = sum(weight for choice, weight in choices)
+   rand_val = random.uniform(0, total_weight)
+   cutoff = 0
+   for choice, weight in choices:
+      if cutoff + weight >= rand_val:
+         return choice
+      cutoff += weight
+
 def move_defender(tau, i):
     # wait tau seconds
     time.sleep(tau)
     
+    # combine P and choices
+    pchoices = [(0, P[i][0]), (1, P[i][1]), (2, P[i][2]), (3, P[i][3])]
+	
     # get next state
-    j = random.choice([0, 1, 2, 3])
-    
+    j = make_choice(pchoices)    
+
     command = './' + names[i] + '_to_' + names[j] + '.sh'
     os.system(command)
     
